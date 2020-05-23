131459cb9ba2208f06e71c777b0c4ae6f1570958
lenovo <lenovo@LAPTOP-0H44TNST>
2020-05-23 09:47:10 +0800

    更新

---
+++ SQLQuery1.sql
@@ -0,0 +1,20 @@
+create database E_commerce
+go
+use E_commerce
+
+create table UserInfo
+(
+	Id int primary key identity,
+	Uname  varchar(500),     --账号
+	RealName   varchar(500),   --真实姓名
+	Pass    varchar(5000),    --密码
+	Email    varchar(5000),    --电子邮件
+	Phone    varchar(5000),    --手机号
+)
+insert into UserInfo values('admin','顾中豪','000','1427865035@qq.com','15030305695')
+
+select * from UserInfo
+
+
+
+select count(*) from UserInfo where Uname='admin' and Pass= '000'
